82af517e5ab838a6ba5210f8e8662da98fef87c4
Roberto Zagnoni <roberto.zagnoni@ethiclab.it>
2021-12-27 19:48:36 +0100

    Update 006-install-pega.sh

--- 006-install-pega.sh
+++ 006-install-pega.sh
@@ -14,7 +14,5 @@ scp postgresql-42.3.1.jar $CONNECTION:/tmp
 ./skeleton.py -t pg_hba.conf.template > pg_hba.conf
 
 scp setupDatabase.properties $CONNECTION:/tmp/116967_Pega8.53/scripts 
-scp pg_hba.conf $CONNECTION:/home/postgres/11/data
-scp postgresql.conf $CONNECTION:/home/postgres/11/data
 
 ssh $CONNECTION 'nohup /tmp/116967_Pega8.53/scripts/install.sh &'
