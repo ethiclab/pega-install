bd457e4d5f1bcae5f542ec1748e6356ab1aace57
Roberto Zagnoni <roberto.zagnoni@ethiclab.it>
2021-12-27 18:42:29 +0100

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,4 +1,4 @@
 drop database if exists pegadb;
 drop user if exists baseuser;
 create user baseuser password 'password';
-create database pegadb owner baseuser lc_collate 'it_IT.UTF-8' lc_ctype 'it_IT.UTF-8' encoding UTF8;
+create database pegadb owner baseuser;
