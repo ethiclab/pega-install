3d4ba540c8c4812087ff9c4e87e8544e556ecdd5
Roberto Zagnoni <r.zagnoni@gmail.com>
2021-12-27 17:30:46 +0100

    Aggiunta installazione del comando zip necessario a sdkman

--- 002-install-sdkman.sh
+++ 002-install-sdkman.sh
@@ -5,5 +5,6 @@ function usage {
 }
 [[ -z "$1" ]] && usage
 export CONNECTION="$1"
+ssh $COONECTION 'sudo dnf -qy install zip'
 ssh $CONNECTION 'sdk version || curl -s "https://get.sdkman.io" | bash'
 ssh $CONNECTION 'java --version || sdk install java 11.0.12-open'
