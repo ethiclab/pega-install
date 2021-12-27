bbaff01f8ef9637f91da025b72ed729f12754444
Roberto Zagnoni <roberto.zagnoni@ethiclab.it>
2021-12-28 00:13:44 +0100

    Update 003-install-postgresql.sh
    rimosso il comando sudo. 
    Dato che è necessario copiare i file pg_hba.conf e postgresql.conf, utilizzando l'utente pegauser, si ha un problema di permission.
    Utilizzando root per tutti i comandi si evita il problema

--- 003-install-postgresql.sh
+++ 003-install-postgresql.sh
@@ -6,16 +6,16 @@ function usage {
 [[ -z "$1" ]] && usage
 echo "003 - INSTALL POSTGRESQL"
 export CONNECTION="$1"
-ssh $CONNECTION 'sudo firewall-cmd --permanent --add-port=5432/tcp'
-ssh $CONNECTION 'sudo firewall-cmd --reload'
-ssh $CONNECTION 'sudo dnf install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-8-x86_64/pgdg-redhat-repo-latest.noarch.rpm'
-ssh $CONNECTION 'sudo dnf -qy module disable postgresql'
-ssh $CONNECTION 'sudo dnf install -y postgresql11-server'
-ssh $CONNECTION 'sudo /usr/pgsql-11/bin/postgresql-11-setup initdb'
+ssh $CONNECTION 'firewall-cmd --permanent --add-port=5432/tcp'
+ssh $CONNECTION 'firewall-cmd --reload'
+ssh $CONNECTION 'dnf install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-8-x86_64/pgdg-redhat-repo-latest.noarch.rpm'
+ssh $CONNECTION 'dnf -qy module disable postgresql'
+ssh $CONNECTION 'dnf install -y postgresql11-server'
+ssh $CONNECTION '/usr/pgsql-11/bin/postgresql-11-setup initdb'
 ./skeleton.py -t pg_hba.conf.template > pg_hba.conf
 scp pg_hba.conf $CONNECTION:/var/lib/pgsql/11/data
 scp postgresql.conf $CONNECTION:/var/lib/pgsql/11/data
-ssh $CONNECTION 'sudo systemctl enable postgresql-11'
-ssh $CONNECTION 'sudo systemctl start postgresql-11'
+ssh $CONNECTION 'systemctl enable postgresql-11'
+ssh $CONNECTION 'systemctl start postgresql-11'
 echo "Create password for user postgres"
-ssh $CONNECTION 'sudo passwd postgres'
+ssh $CONNECTION 'passwd postgres'
