2afe88625c8de501efeb7188c8815a1a80d33181
Roberto Zagnoni <r.zagnoni@gmail.com>
2021-12-28 00:48:27 +0100

    fix tomcat context.xml

--- 009-deploy-pega.sh
+++ 009-deploy-pega.sh
@@ -9,6 +9,7 @@ export CONNECTION="$1"
 scp postgresql-42.3.1.jar $CONNECTION:/usr/share/tomcat/lib
 scp tomcat-users.xml $CONNECTION:/usr/share/tomcat/conf/tomcat-users.xml
 scp prweb.war $CONNECTION:/usr/share/tomcat/webapps
+./skeleton.py -t context.xml.template > context.xml
 scp context.xml $CONNECTION:/usr/share/tomcat/conf
 ssh $CONNECTION 'systemctl stop tomcat'
 ssh $CONNECTION 'systemctl start tomcat'
