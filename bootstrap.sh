6a6930fe91b68788b2ce0b28d081eca31830ac17
Montoya Edu <montoya.edu@gmail.com>
2021-12-27 14:13:11 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
-echo hello
+yum update
+dnf -qy install unzip
+dnf -qy module install python27
+ln -s /usr/bin/python2 /usr/bin/python
+curl -O -L https://github.com/ethiclab/pega-install/archives/refs/1.0.zip
+unzip 1.0.zip
+cd pega-install-1.0
+source install.sh
