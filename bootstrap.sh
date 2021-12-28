8aa4dabdfaeb20996b05af7ae8fef42423adfd75
Montoya Edu <montoya.edu@gmail.com>
2021-12-28 13:52:46 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,5 +8,11 @@ python2 -m pip install --user Cheetah
 curl -O -L https://github.com/ethiclab/pega-install/archive/refs/tags/${VERSION}.zip
 unzip ${VERSION}.zip
 cd pega-install-${VERSION}
-source install.sh
-echo "Start installation with command: pega_install"
+echo "Configure config.json accordingly and then do:"
+echo ""
+echo "    cd pega-install-${VERSION}"
+echo "    source install.sh"
+echo ""
+echo "then, start installation with command:"
+echo ""
+echo "    pega_install"
