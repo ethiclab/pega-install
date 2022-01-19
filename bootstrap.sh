6584dfe0119ae012dc527036c5a9eaa54ed5c5ad
Montoya Edu <montoya.edu@gmail.com>
2022-01-19 13:25:25 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,18 +2,21 @@
 function log_info {
   >&2 echo $@
 }
-log_info bootstrap version 2.0
+log_info bootstrap version 2.1
 function throw {
   >&2 echo $@
   exit 1
 }
+if [ "$EUID" -ne 0 ]
+  then export SUDO=sudo
+fi
 VERSION=1.20
 [[ ! -x $(which yum) ]] && throw yum not found
 [[ ! -x $(which dnf) ]] && throw dnf not found
-yum update -qy || throw error
-dnf -qy install unzip || throw error
-dnf -qy module install python27 || throw error
-ln -f -s /usr/bin/python2 /usr/bin/python
+$SUDO yum update -qy || throw error
+$SUDO dnf -qy install unzip || throw error
+$SUDO dnf -qy module install python27 || throw error
+$SUDO ln -f -s /usr/bin/python2 /usr/bin/python
 python2 -m pip install --user Cheetah3
 curl -O -L https://github.com/ethiclab/pega-install/archive/refs/tags/${VERSION}.zip
 unzip ${VERSION}.zip
